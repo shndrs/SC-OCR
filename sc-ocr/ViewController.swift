7bc643cfa528bb5475b28caca6219b7188cefadb
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-02-06 10:51:03 +0330

    Update ViewController fillOpaqueAroundAreaOfInterest procedure

--- sc-ocr/ViewController.swift
+++ sc-ocr/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import SwiftOCR
+import AVFoundation
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    private lazy var excludeLayer: CAShapeLayer = {
+        return CAShapeLayer()
+    }()
+    
+    @IBOutlet private weak var cameraView: UIView!
+    @IBOutlet private weak var interestRegion: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let swiftOCRInstance = SwiftOCR()
+        
+        let img = UIImage(named: "melli-example")!
+        
+        swiftOCRInstance.recognize(img) { recognizedString in
+            print(recognizedString)
+        }
+        
     }
-
-
+    
+    
 }
 
+// MARK: Methods
+
+extension ViewController {
+    private func fillOpaqueAroundAreaOfInterest(parentView: UIView, areaOfInterest: UIView) {
+        let parentViewBounds = parentView.bounds
+        let areaOfInterestFrame = areaOfInterest.frame
+        
+        let path = UIBezierPath(rect: parentViewBounds)
+        let areaOfInterestPath = UIBezierPath(rect: areaOfInterestFrame)
+        path.append(areaOfInterestPath)
+        path.usesEvenOddFillRule = true
+        
+        excludeLayer.path = path.cgPath
+        parentView.layer.addSublayer(excludeLayer)
+    }
+}
+
+// MARK: - Life Cycle
+
+extension ViewController {
+}
