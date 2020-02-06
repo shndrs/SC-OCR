1822de7e2401fd4a61d303d4c3f845cbf20bcc0d
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-02-06 10:27:59 +0330

    First commit

---
+++ sc-ocr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sc-ocr
+//
+//  Created by NP2 on 2/6/20.
+//  Copyright © 2020 shndrs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
